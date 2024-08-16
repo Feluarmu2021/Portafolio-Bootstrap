9b35ea3bb2eecd575756394fcca8956d39ef6e7d
Felipe Aros Muñoz <feluarmu2021@gmail.com>
2024-08-16 16:37:48 -0400

    Primer portafolio en base a Bootstrap

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function(){
+    function escribir(){
+        let texto = document.getElementById('escrito');
+        texto.innerHTML = 'futuro FullStack JavaScript Trainee';
+       
+    }
+    
+    escribir();
+
+})    
